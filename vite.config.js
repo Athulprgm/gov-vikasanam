53d348dcc739e35470f02c925c5dea502f6d9722
Athul Krishna <athulkrishna.cpd@gmail.com>
2026-05-26 13:24:24 +0530

    Patch dns.lookup in vite.config.js to force custom DNS resolution for Railway proxy requests

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,33 @@ import tailwindcss from '@tailwindcss/vite'
 // Set DNS servers to Google (8.8.8.8) and Cloudflare (1.1.1.1) to bypass Jio ISP DNS block
 dns.setServers(['8.8.8.8', '1.1.1.1'])
 
+// Monkey-patch dns.lookup since Node's http.request/https.get uses dns.lookup which ignores dns.setServers()
+const originalLookup = dns.lookup
+dns.lookup = function (hostname, options, callback) {
+  if (typeof options === 'function') {
+    callback = options
+    options = {}
+  }
+  
+  if (hostname.endsWith('.up.railway.app')) {
+    dns.resolve4(hostname, (err, addresses) => {
+      if (err || !addresses || !addresses.length) {
+        return originalLookup(hostname, options, callback)
+      }
+      
+      const ip = addresses[0]
+      if (options.all) {
+        return callback(null, [{ address: ip, family: 4 }])
+      } else {
+        return callback(null, ip, 4)
+      }
+    })
+    return
+  }
+  
+  return originalLookup(hostname, options, callback)
+}
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(), react()],
